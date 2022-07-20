0280978112011260b201a7ecf1fa1729bceacfea
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-07-20 19:11:22 +0100

    Moved Clarifai to backend and route fixes

--- server.js
+++ server.js
@@ -19,24 +19,23 @@ const db = knex({
     }
 });
 
-db.select('*').from('users').then(data => {
-    console.log(data);
-})
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-
 app.get('/', (req, res)=> {
-    res.send('success');
+    res.send(database.users);
 })
 
-app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
+// app.post('/signin', signin.handleSignIn(db, bcrypt)) // alternate way of writing it
+app.post('/signin', signin.handleSignIn(db, bcrypt))
+// app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id',(req, res) => {profile.handleProfile(req, res, db)} )
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
 app.listen(3000, ()=> {
     console.log("app is running on port 3000");
