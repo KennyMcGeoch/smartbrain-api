401132514b3d582548386ee36e5066767f147469
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-08-23 15:34:17 +0100

    updated connectionString code

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ const profile = require('./Controllers/profile');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_url,
-        ssl: true,
-    }
-});
+        connectionString: process.env.DATABASE_URL,
+ssl: {
+rejectUnauthorized: false,
+},
+}});
 
 
 const app = express();
