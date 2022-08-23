5524650d4307cd6dce634727ecf884df40de81a3
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-08-23 17:15:45 +0100

    Removed API key from codebase and added it as an environmental variable

--- Controllers/image.js
+++ Controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: 'ba619e535c3d41568ea89cf92ceb3916'
+    apiKey: process.env.API_CLARIFAI
   });
 
 const handleApiCall = (req, res) => {
