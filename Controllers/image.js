c6724275ba05ce0bd59dc013537840562af70b1d
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2023-01-25 18:56:31 +0000

    API Key test 2

--- Controllers/image.js
+++ Controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 const process = require('process');
 
 const app = new Clarifai.App({
-    apiKey: "a619e535c3d41568ea89cf92ceb3916"
+    apiKey: process.env.CLARIFAI_KEY
   });
 
 const handleApiCall = (req, res) => {
