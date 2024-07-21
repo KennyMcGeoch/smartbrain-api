8d0cf7af7cac01b96569edca1e820e4ca3b403ea
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-07-21 20:07:26 +0100

    err display

--- Controllers/signin.js
+++ Controllers/signin.js
@@ -18,7 +18,7 @@ const handleSignIn = (db, bcrypt) => (req, res) => {
         else{
             res.status(400).json('Wrong credentials')
         }
-    }).catch(err => res.status(400).json('Wrong credentials, random'))
+    }).catch(err => res.status(400).json('Wrong credentials', err))
 }
 
 module.exports = {
