5e89954632f21a6081c4910eba5658e9ceb2daf1
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2024-07-22 11:55:38 +0100

    Debug

--- Controllers/signin.js
+++ Controllers/signin.js
@@ -18,7 +18,7 @@ const handleSignIn = (db, bcrypt) => (req, res) => {
         else{
             res.status(400).json('Wrong credentials')
         }
-    }).catch(err => res.status(400).json('Wrong credentials'))
+    }).catch(err => res.status(400).json(err))
 }
 
 module.exports = {
